2aaf1957fab1c2e361dbc0cb865bee3572a82f3b
SamuelBrown2 <55718359+SamuelBrown2@users.noreply.github.com>
2019-09-23 14:32:09 -0700

    Sam code rewrite

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -5,65 +5,186 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
+
 #include "Robot.h"
-
 #include <iostream>
-
+#include <string>
+#include <frc/XboxController.h>
+#include <frc/IterativeRobot.h>
+#include <frc/smartdashboard/SendableChooser.h>
+#include <frc/SpeedControllerGroup.h>
+#include <frc/GenericHID.h>
+#include <frc/Joystick.h>
+#include <frc/Timer.h>
+#include <frc/IterativeRobot.h>
+#include <frc/Spark.h>
+#include <frc/SpeedController.h>
+#include <frc/DoubleSolenoid.h>
+#include <frc/Solenoid.h>
+#include <frc/Spark.h>
+#include <frc/Compressor.h>
+#include <frc/drive/DifferentialDrive.h>
 #include <frc/smartdashboard/SmartDashboard.h>
 
+using namespace frc;
+ 
+// This function is nulled untill a opurtunity to use it arises
+ 
+/*
+void Robot::SecondTimer(frc::Timer *timer, float value)
+{
+ 
+   this->timer = *timer;
+   timer->Reset(); // resets the time back to Zero
+ 
+   if(timer->HasPeriodPassed(value)) // If the value  seconds have passed return a true statement
+   {
+     timer->Stop(); // Stop the timer
+   }
+ 
+}
+
+*/
+
+// Turns off the solenoids two seconds before the match ends
+ 
+void Robot::TurnOffSolenoids (frc::Timer *Timer, frc::DoubleSolenoid *dossol, frc::DoubleSolenoid *tressol)
+{
+ 
+   if (Timer->HasPeriodPassed(148))
+     {
+           dossol->~DoubleSolenoid(); // Calls the deconstructor of the dossol double solenoid turning it off
+           tressol->~DoubleSolenoid(); // Calls the deconstructor of the tresol double solenoid turning it off
+     }
+}
+ 
+ 
 void Robot::RobotInit() {
-  m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
-  m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
-  frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
+ m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
+ m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
+ frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
+ 
+ //This starts the Camera Instance.sdf
+   /*CameraServer.getInstance().startAutomaticCapture();
+  */
+   frc::DifferentialDrive m_myRobot{*m_leftGroup, *m_m_controllerGroup};
+   frc::XboxController *m_controller = new frc::XboxController(0); //the number is the port in the Driver Station
 }
-
+ 
 /**
- * This function is called every robot packet, no matter the mode. Use
- * this for items like diagnostics that you want ran during disabled,
- * autonomous, teleoperated and test.
- *
- * <p> This runs after the mode specific periodic functions, but before
- * LiveWindow and SmartDashboard integrated updating.
- */
+* This function is called every robot packet, no matter the mode. Use
+* this for items like diagnostics that you want ran during disabled,
+* autonomous, teleoperated and test.
+*
+* <p> This runs after the mode specific periodic functions, but before
+* LiveWindow and SmartDashboard integrated updating.
+*/
 void Robot::RobotPeriodic() {}
-
+ 
 /**
- * This autonomous (along with the chooser code above) shows how to select
- * between different autonomous modes using the dashboard. The sendable chooser
- * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
- * remove all of the chooser code and uncomment the GetString line to get the
- * auto name from the text box below the Gyro.
- *
- * You can add additional auto modes by adding additional comparisons to the
- * if-else structure below with additional strings. If using the SendableChooser
- * make sure to add them to the chooser code above as well.
- */
+* This autonomous (along with the chooser code above) shows how to select
+* between different autonomous modes using the dashboard. The sendable chooser
+* code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
+* remove all of the chooser code and uncomment the GetString line to get the
+* auto name from the text box below the Gyro.
+*
+* You can add additional auto modes by adding additional comparisons to the
+* if-else structure below with additional strings. If using the SendableChooser
+* make sure to add them to the chooser code above as well.
+*/
 void Robot::AutonomousInit() {
-  m_autoSelected = m_chooser.GetSelected();
-  // m_autoSelected = SmartDashboard::GetString("Auto Selector",
-  //     kAutoNameDefault);
-  std::cout << "Auto selected: " << m_autoSelected << std::endl;
-
-  if (m_autoSelected == kAutoNameCustom) {
-    // Custom Auto goes here
-  } else {
-    // Default Auto goes here
-  }
+ m_autoSelected = m_chooser.GetSelected();
+ // m_autoSelected = SmartDashboard::GetString(
+ //     "Auto Selector", kAutoNameDefault);
+ std::cout << "Auto selected: " << m_autoSelected << std::endl;
+ 
+ if (m_autoSelected == kAutoNameCustom) {
+   // Custom Auto goes here
+ } else {
+   // Default Auto goes here
+ }
+ 
+ /*
+   compressor->SetClosedLoopControl(true);
+   compressor->SetClosedLoopControl(false);
+   bool enabled = compressor->Enabled();
+   bool pressureSwitch = compressor->GetPressureSwitchValue();
+   double current = compressor->GetCompressorCurrent();
+   */
+ 
 }
-
+ 
 void Robot::AutonomousPeriodic() {
-  if (m_autoSelected == kAutoNameCustom) {
-    // Custom Auto goes here
-  } else {
-    // Default Auto goes here
-  }
+ if (m_autoSelected == kAutoNameCustom) {
+   // Custom Auto goes here
+ } else {
+   // Default Auto goes here
+ }
 }
-
+ 
 void Robot::TeleopInit() {}
+ 
+void Robot::TeleopPeriodic() {
 
-void Robot::TeleopPeriodic() {}
+ Timer *timer; // Starts a timer instance for use of the turn off solenoids function
 
+   TurnOffSolenoids(timer,dosSol,tresSol);
+
+   frc::XboxController* m_controller(0);
+   frc::DifferentialDrive m_myRobot{*m_leftGroup, *m_m_controllerGroup};
+ 
+ //LDTMotor = m_controller.getY(Hand.km_controller);
+   //RDTMotor = m_controller.getY(Hand.kLeft);
+  
+   //m_myRobot.tankDrive(RDTMotor/1.3, LDTMotor/1.3);
+   //m_myRobot.tankDrive(m_controller.getY(), m_controller.getX());
+
+   if (m_controller->GetRawButton(kGamepadButtonStart))
+   {
+   compressor->SetClosedLoopControl(true);
+   compressor->SetClosedLoopControl(false);
+   bool enabled = compressor->Enabled();
+   bool pressureSwitch = compressor->GetPressureSwitchValue();
+   double current = compressor->GetCompressorCurrent();
+   }
+   if(m_controller->GetRawButton(kGamepadButtonA)==true)
+   {
+     dosSol->Set(DoubleSolenoid::kForward); //
+   }
+   if(m_controller->GetRawButton(kGamepadButtonB)==true)
+   {
+     dosSol->Set(frc::DoubleSolenoid::kReverse);
+   }
+   if(m_controller->GetRawButton(kGamepadButtonX)==true)
+   {
+     tresSol->Set(frc::DoubleSolenoid::kForward); //
+   }
+   if(m_controller->GetRawButton(kGamepadButtonY)==true)
+   {
+     tresSol->Set(frc::DoubleSolenoid::kReverse);
+   }
+   if(m_controller->GetY(GenericHID::kLeftHand) <= -.5 && m_controller->GetY(GenericHID::kLeftHand) != 0 ){
+     m_myRobot.TankDrive(-m_controller->GetY(GenericHID::kLeftHand),-m_controller->GetY(GenericHID::kLeftHand));
+   } //Front
+   else if (m_controller->GetY(GenericHID::kLeftHand) >= .5 && m_controller->GetY (GenericHID::kLeftHand)!= 0){
+     m_myRobot.TankDrive( - (m_controller->GetY(GenericHID::kLeftHand)), -( m_controller->GetY(GenericHID::kLeftHand)) );
+   } //Back
+  
+   else if(m_controller->GetX(GenericHID::kLeftHand) <= .5 && m_controller->GetX(GenericHID::kLeftHand) != 0 ){
+     m_myRobot.TankDrive(0,-( m_controller->GetX(GenericHID::kLeftHand) ) );
+   } //Left
+  
+   else if (m_controller->GetX(GenericHID::kLeftHand) >= -.5 && m_controller->GetX (GenericHID::kLeftHand)!= 0){
+     m_myRobot.TankDrive(m_controller->GetX(GenericHID::kLeftHand), 0);
+   } //right
+   else {
+     m_myRobot.TankDrive(0,0);
+   }//Off0
+ 
+}
+ 
 void Robot::TestPeriodic() {}
+ 
 
 #ifndef RUNNING_FRC_TESTS
 int main() { return frc::StartRobot<Robot>(); }
--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -4,27 +4,93 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
+ 
 #pragma once
-
+ 
 #include <string>
-
-#include <frc/TimedRobot.h>
+#include <frc/XboxController.h>
+#include <frc/IterativeRobot.h>
 #include <frc/smartdashboard/SendableChooser.h>
+#include <frc/SpeedControllerGroup.h>
+#include <frc/GenericHID.h>
+#include <frc/Joystick.h>
+#include <frc/Timer.h>
+#include <frc/IterativeRobot.h>
+#include <frc/Spark.h>
+#include <frc/SpeedController.h>
+#include <frc/DoubleSolenoid.h>
+#include <frc/Solenoid.h>
+#include <frc/Spark.h>
+#include <frc/Compressor.h>
+#include <frc/Timer.h>
+ 
+ 
+ 
+ 
+class Robot : public frc::IterativeRobot {
+public:
+ void RobotInit() override;
+ void RobotPeriodic() override;
+ void AutonomousInit() override;
+ void AutonomousPeriodic() override;
+ void TeleopInit() override;
+ void TeleopPeriodic() override;
+ void TestPeriodic() override;
+ 
+ 
+ //0 is m_controller whilst 1 is left for the drive motors.
+ static int kControllerPort;
+ static int kMotorPortArm;
+ static int kMotorPortClaw;
+ static int kGamepadButtonLogitech;
+ bool buttonValue;
+ 
+ static int kMotorPortFrontLeft;
+ static int kMotorPortRearLeft;
+ static int kMotorPortFrontm_controller;
+ static int kMotorPortRearm_controller;
+ 
+ enum ControllerButtons : int const {
+kGamepadButtonA = 1,         // A button
+kGamepadButtonB = 2,         // B button
+kGamepadButtonX = 3,         // X button
+kGamepadButtonY = 4,         // Y button
+kGamepadButtonShoulderL = 5, // Left Bumper
+kGamepadButtonShoulderR = 6, // Right Bumper
+kGamepadButtonBack = 7,      // Back button
+kGamepadButtonStart = 8,     // Start Button
+kGamepadButtonLeftStick = 9, 
+kGamepadButtonm_controllerStick = 10,
+kGamepadButtonMode = -1,
+ 
+ };
 
-class Robot : public frc::TimedRobot {
- public:
-  void RobotInit() override;
-  void RobotPeriodic() override;
-  void AutonomousInit() override;
-  void AutonomousPeriodic() override;
-  void TeleopInit() override;
-  void TeleopPeriodic() override;
-  void TestPeriodic() override;
+    
+ double LDTMotor;
+ double RDTMotor;
+ 
+ frc::Spark *m_frontLeft = new frc::Spark(kMotorPortFrontLeft);
+ frc::Spark *m_rearLeft = new frc::Spark(kMotorPortRearLeft);
+ frc::SpeedController *m_leftGroup = new frc::SpeedControllerGroup(*m_rearLeft, *m_frontLeft);
+ frc::Spark *m_frontm_controller = new frc::Spark(kMotorPortFrontm_controller);
+ frc::Spark *m_rearm_controller = new frc::Spark(kMotorPortRearm_controller);
+ frc::SpeedControllerGroup *m_m_controllerGroup = new frc::SpeedControllerGroup(*m_rearm_controller, *m_frontm_controller);
+ frc::Spark *m_arm = new frc::Spark(kMotorPortArm);
+ frc::DoubleSolenoid *dosSol = new frc::DoubleSolenoid(0,1);
+ frc::DoubleSolenoid *tresSol = new frc::DoubleSolenoid (6,7);
+ frc::Compressor *compressor;
+ 
+ 
+ 
+private:
+ 
+ frc::SendableChooser<std::string> m_chooser;
+ const std::string kAutoNameDefault = "Default";
+ const std::string kAutoNameCustom = "My Auto";
+ std::string m_autoSelected;
+   // void SecondTimer(); This function is null until it is needed
 
- private:
-  frc::SendableChooser<std::string> m_chooser;
-  const std::string kAutoNameDefault = "Default";
-  const std::string kAutoNameCustom = "My Auto";
-  std::string m_autoSelected;
+
+  void TurnOffSolenoids (frc::Timer *Timer, frc::DoubleSolenoid *dossol, frc::DoubleSolenoid *tressol);
+
 };
